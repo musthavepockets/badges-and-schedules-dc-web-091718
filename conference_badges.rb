c4366dc6252cfe89c54d07eaaecd482953f02574
cd <coraldarling@outlook.com>
2018-09-03 16:42:21 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,6 +11,6 @@ def batch_badge_creator(speakers)
 end 
 
 def assign_rooms(guests)
-  guests.collect { |guest, index| "Hello, #{guest}! You'll be assigned to room #{index + 1}!"}
+  guests.collect { |guest, index| "Hello, #{guest}! You'll be assigned to room #{index}!"}
   guests
 end
